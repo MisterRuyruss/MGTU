3cb4206373dd35a325aa4e2239f940d93c82080f
Ruyruss <71020590+MisterRuyruss@users.noreply.github.com>
2025-09-06 16:59:36 +0300

    Добавление комментов и пояснений

--- Lab_OAiP/lab1/lab1task1/lab1task1.cpp
+++ Lab_OAiP/lab1/lab1task1/lab1task1.cpp
@@ -2,7 +2,6 @@
 
 
 #include <iostream>
-
 using namespace std;
 
 
@@ -13,22 +12,26 @@ int main()
     int suma = 0;
     int max = -10 ^ 10;
 
+    // Ввод массива с клавиатуры
     for (int i = 0; i < size; i++) {
         cout << "Введите " << i + 1 << " элемент: ";
         cin >> array[i];
     }
-    cout << "Ваш введённый масив:" << endl;
 
+    // Вывод в одну строку всего массива
+    cout << "Ваш введённый масив:" << endl;
     for (int i = 0; i < size; i++) {
         cout << array[i] << " ";
     }
     cout << endl;
 
+    // Сумма элементов массива
     for (int i = 0; i < size; i++) {
         suma += array[i];
     }
     cout << "Сумма элементов вашего массива: " << suma << endl;
     
+    // Нахождение максимального элемента массива
     for (int i = 0; i < size; i++) {
         if (array[i] > max) {
             max = array[i];
@@ -36,6 +39,7 @@ int main()
     }
     cout << "Максимальный элемент вашего массива: " << max << endl;
 
+    // Сортировка массива методом вставки
     for (int i = 0; i < size; i++) {
         for (int j = i; j > 0 && array[j - 1] < array[j]; j--) {
             swap(array[j], array[j - 1]);
