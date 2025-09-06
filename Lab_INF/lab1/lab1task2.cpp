4c90fc473ba3623cd17bfbab702533521b609e2d
Ruyruss <71020590+MisterRuyruss@users.noreply.github.com>
2025-09-06 15:13:12 +0300

    Изменены названия файлов на lab1task1

---
+++ Lab_INF/lab1/lab1task2.cpp
@@ -0,0 +1,23 @@
+// Вариант 4
+// Номер 2
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x;
+	int kol = 1;
+	cout << "Сколько машин можно произвести: ";
+	
+	cin >> x;
+
+	while(x > 0){
+		kol *= x
+		x -= 1
+	}
+	cout << "Производительность: " << kol
+	
+
+
+}
